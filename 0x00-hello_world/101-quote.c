fe8be3e400e65d71dc414cbf336138529985df31
underscoDe <developer.awounou@gmail.com>
2021-08-26 10:47:58 +0100

    you need to be a genius

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return : return 1
+ */
+
+int main(void)
+{
+fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
