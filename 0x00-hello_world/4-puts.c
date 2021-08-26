bb0ef634e501c7d9c6415992a667ebb80eb30904
underscoDe <developer.awounou@gmail.com>
2021-08-26 09:35:47 +0100

    hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main : entry point
+ * main: Entry point
  *
- * return 0
+ * Return: always 0
  *
  */
 
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
