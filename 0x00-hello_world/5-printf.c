46978c047fa3735ed6c9b0ad31ec74fb08701ef6
underscoDe <developer.awounou@gmail.com>
2021-08-26 10:21:40 +0100

    hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: returns 0
+ *
+ */
+
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+}
