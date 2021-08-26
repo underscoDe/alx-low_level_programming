80dd7aa17eae2051ad561caeed4fa45ba0aa1396
underscoDe <developer.awounou@gmail.com>
2021-08-26 10:52:11 +0100

    you need to be a genius

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,7 @@
 
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+char error = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+fprintf(stderr, "%s", error);
 return (1);
 }
