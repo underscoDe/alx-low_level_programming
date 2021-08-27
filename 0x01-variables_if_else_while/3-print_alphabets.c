05501323e7afb94288083aca623145c0bae783d5
underscoDe <developer.awounou@gmail.com>
2021-08-27 19:36:57 +0100

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,12 +9,12 @@
 
 int main(void)
 {
-    int n;
+	int n;
 
-    for (n = 97; n <= 122; ++n)
-        putchar(n);
-    for (n = 65; n <= 90; ++n)
-        putchar(n);
-    putchar(10);
-    return (0);
+	for (n = 97; n <= 122; ++n)
+		putchar(n);
+	for (n = 65; n <= 90; ++n)
+		putchar(n);
+	putchar(10);
+	return (0);
 }
