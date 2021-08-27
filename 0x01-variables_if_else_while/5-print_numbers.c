3c9072252380d8e3d7f3ebb921ccd6bad7ab9f28
underscoDe <developer.awounou@gmail.com>
2021-08-27 19:57:13 +0100

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: returns 0 when everything works fine
+*
+*/
+
+int main(void)
+{
+    int n;
+
+    for (n = 0; n < 10; ++n)
+    {
+        printf("%n\n");
+    }
+    return (0);
+}
