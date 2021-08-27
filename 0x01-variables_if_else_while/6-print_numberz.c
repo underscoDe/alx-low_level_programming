827fec3ae7e2079e10402be4123585b9a6c82ddf
underscoDe <developer.awounou@gmail.com>
2021-08-27 20:06:16 +0100

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: returns 0 when everything works fine
+*
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; ++n)
+	{
+		putchar(n);
+	}
+	putchar(10);
+	return (0);
+}
