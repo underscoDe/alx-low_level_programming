dc46e6ef8e6b2828b7f29f3aba11beb289b778b3
underscoDe <developer.awounou@gmail.com>
2021-08-27 19:27:33 +0100

    alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: returns 0 when everything works fine
+*
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 97; n <= 122; ++n)
+		putchar(n);
+	putchar(10);
+	return (0);
+}
