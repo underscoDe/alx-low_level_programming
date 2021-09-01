9d72d5a715d2bc7eef5bdb6503955860d4d82a0a
underscoDe <developer.awounou@gmail.com>
2021-09-01 12:08:32 +0100

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+/**
+ * _abs - computes the absolute value of a number
+ * @n: number to be computed
+ *
+ * Return: the absolute value of the number
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
