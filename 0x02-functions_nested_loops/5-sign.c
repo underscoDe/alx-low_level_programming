09726b3bf998390f0e58f8aa744eee1bf7aee43a
underscoDe <developer.awounou@gmail.com>
2021-09-01 12:41:10 +0100

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,28 +1,24 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * print_sign - checks whether a number is negative, 0, or positive
- * @n: the number to be checked
- *
- * Return: 1 if n is positive
- * 0 if n is zero
- * -1 if n is negative
- */
+*print_sign-prints sign of a number
+*@n: character to be verified
+*Return: return 0 or 1
+*/
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-	else
-	{
-		_putchar('-');
-		return (-1);
-	}
+if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
 }
