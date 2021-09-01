383116fddf62be8d12c14532a1856f19bb5598ed
underscoDe <developer.awounou@gmail.com>
2021-09-01 12:42:29 +0100

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,51 +1,28 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * times_table - prints the 9 times table
- *
- * Return: Doesn't return anything, void
- */
+*times_table - prints the 9 times table
+*
+*Return: returns nothing
+*/
 void times_table(void)
 {
-	int i, j, product, x;
-
-	for (j = 0; j <= 9; j++)
-	{
-		for (i = 0; i <= 9; i++)
-		{
-			product = j * i;
-			if (i != 0)
-			{
-				_putchar(',');
-				for (x = len(product); x <= len(9 * 9); x++)
-					_putchar(' ');
-			}
-			if (product > 9)
-			{
-				_putchar(product / 10 + '0');
-			}
-			_putchar(product % 10 + '0');
-		}
-		_putchar('\n');
-	}
-}
-
-/**
- * len - counts the number of digits in a number
- * @n: number to be used
- *
- * Return: returns the number of digits
- */
-int len(int n)
+int digit, mult, result;
+for (digit = 0; digit <= 9; digit++)
 {
-	int out = 0;
+_putchar('0');
 
-	if (n == 0)
-		return (1);
-	while (n != 0)
-	{
-		n /= 10;
-		out++;
-	}
-	return (out);
+for (mult = 1; mult <= 9; mult++)
+{
+_putchar(',');
+_putchar(' ');
+result = digit * mult;
+if (result <= 9)
+_putchar(' ');
+else
+_putchar((result / 10) + '0');
+
+_putchar((result % 10) + '0');
+}
+_putchar('\n');
+}
 }
