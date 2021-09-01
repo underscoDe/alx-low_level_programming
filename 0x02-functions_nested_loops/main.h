5dfcb0a60d4255eada2d01ff661f53a9b6480d6e
underscoDe <developer.awounou@gmail.com>
2021-09-01 12:30:16 +0100

    added main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+/**
+ *File: main.h
+ *Author: Arsene Awounou
+ *Desc: header file
+ */
+
+void print_alphabet(void);
+void jack_bauer(void);
+void print_alphabet_x10(void);
+void times_table();
+void print_to_98(int);
+void print_times_table(int);
+int _putchar(char);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int add(int n, int m);
+int print_last_digit(int n);
