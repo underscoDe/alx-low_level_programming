f851f0cc48310cd07ab2c640496f41fc8b8be8d8
underscoDe <developer.awounou@gmail.com>
2021-09-01 12:06:42 +0100

    _putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point, prints ""Holberton" to the standard output
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar(10);
+	return (0);
+}
