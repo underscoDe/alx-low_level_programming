b307086a446713a2a3e2ad976190a5f3b230a468
underscoDe <developer.awounou@gmail.com>
2021-09-02 14:50:54 +0100

    multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * main - Entry point
- * Return: 0
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
-	int i;
+int i;
 
-	i = 0;
-	positive_or_negative(i);
+i = 0;
+positive_or_negative(i);
 
-	return (0);
+return (0);
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,5 +4,8 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
+int largest_number(int, int, int);
+void print_remaining_days(int, int, int);
+int convert_day(int, int);
 
 #endif /* MAIN_H */
