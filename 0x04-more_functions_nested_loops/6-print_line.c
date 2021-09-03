e0c034f193b74034edc40fdecf359079323d5834
underscoDe <developer.awounou@gmail.com>
2021-09-03 19:11:38 +0100

    straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*print_line - prints a straight line
+*@n: parameter
+*Return:returns nothing
+*/
+
+void print_line(int n)
+{
+	while (n-- > 0)
+		_putchar('_');
+	_putchar('\n');
+}
