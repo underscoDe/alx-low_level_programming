42f4cf7c88ed89c358decea9fff0f68a451ce60e
underscoDe <developer.awounou@gmail.com>
2021-09-03 21:57:32 +0100

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,36 +10,35 @@
 
 int main(void)
 {
-    int num = 1;
+	int num = 1;
 
-    while (num++ <= 100)
-    {
-        if ((num % 3 == 0) && (num % 5 == 0))
-        {
-            printf("FizzBuzz ");
-        }
-        else if ((num % 3) == 0)
-        {
-            printf("Fizz ");
-        }
-        else if ((num % 5) == 0)
-        {
-            if (num != 100)
-            {
-                printf("Buzz ");
-            }
+	while (num++ < 100)
+	{
+		if ((num % 3 == 0) && (num % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((num % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((num % 5) == 0)
+		{
+			if (num != 100)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				printf("Buzz");
+			}
+		}
+		else
+		{
+			printf("%d ", num);
+		}
+	}
+	printf("\n");
 
-            else
-            {
-                printf("Buzz");
-            }
-        }
-        else
-        {
-            printf("%d ", num);
-        }
-    }
-
-    printf("\n");
-    return (0);
+	return (0);
 }
