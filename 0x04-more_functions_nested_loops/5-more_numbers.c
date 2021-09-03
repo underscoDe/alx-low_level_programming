119024f5600599220bf084ac1084e57dc9c29163
underscoDe <developer.awounou@gmail.com>
2021-09-03 19:11:23 +0100

    only universal language

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*more_numbers - prints numbers to 14
+*
+*Return: returns nothing
+*/
+
+void more_numbers(void)
+{
+	int n1, n2;
+
+	for (n1 = 0; n1 < 10; n1++)
+	{
+		for (n2 = 0; n2 <= 14; n2++)
+		{
+			if (n2 > 9)
+				_putchar((n2 / 10) + '0');
+			_putchar((n2 % 10) + '0');
+		}
+		_putchar(10);
+	}
+}
