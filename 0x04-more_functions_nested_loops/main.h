e997cd2bba030a7909c4d3f855470a3a27311683
underscoDe <developer.awounou@gmail.com>
2021-09-03 20:31:27 +0100

    added main.h

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+*main - header file for protoypes
+*void protoypes (void)
+*int protoypes (int)
+*/
+
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
+
+#endif /* MAIN_H */
