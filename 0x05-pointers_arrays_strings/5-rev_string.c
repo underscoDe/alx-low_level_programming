2a767471bad804263eb64d81ef6f1e770f1e56c4
underscoDe <developer.awounou@gmail.com>
2021-09-06 18:56:06 +0100

    think in reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,22 +14,22 @@ int _strlen(char *s);
 
 void rev_string(char *s)
 {
-    int i, tmp, len = _strlen(s);
+	int i, tmp, len = _strlen(s);
 
-    for (i = 0; i < len; i++)
-    {
-        tmp = *(s + i);
-        *(s + i) = *(s + len - i - 1);
-        *(s + len - i - 1) = tmp;
-    }
+	for (i = 0; i < len; i++)
+	{
+		tmp = *(s + i);
+		*(s + i) = *(s + len - i - 1);
+		*(s + len - i - 1) = tmp;
+	}
 }
 
 int _strlen(char *s)
 {
-    int len = 0;
+	int len = 0;
 
-    while (*(s + len) != '\0')
-        len++;
+	while (*(s + len) != '\0')
+		len++;
 
-    return (len);
+	return (len);
 }
