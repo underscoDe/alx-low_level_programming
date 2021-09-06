091fb858942ef5a445022ae40e0f0ec07adcbe8a
underscoDe <developer.awounou@gmail.com>
2021-09-06 19:32:33 +0100

    winning is only half of it, having fun is the other half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,9 +10,26 @@
 
 void puts_half(char *str)
 {
-	int i, len = strlen(str);
+	int i, len = _strlen(str);
 
 	for (i = ((len - 1) / 2); i < len; i++)
 		putchar(*(str + i));
 	putchar(10);
+}
+
+/**
+* _strlen - returns the length of a string
+* @s: string
+*
+* Return: the length of the given string
+*/
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
 }
