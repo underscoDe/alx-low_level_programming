ed30cee35d6b19d010f3500cccf5265cb8b035e2
underscoDe <developer.awounou@gmail.com>
2021-09-06 18:48:48 +0100

    think in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*rev_string - Reverses a string
+*@s: string to be reversed
+*Return: Nothing
+*_strlen - returns the length of a string
+*@s: string
+*Return: the length of the given string
+*/
+
+int _strlen(char *s);
+
+void rev_string(char *s)
+{
+    int i, tmp, len = _strlen(s);
+
+    for (i = 0; i < len; i++)
+    {
+        tmp = *(s + i);
+        *(s + i) = *(s + len -i);
+        *(s + len -i) = tmp;
+    }
+}
+
+int _strlen(char *s)
+{
+    int len = 0;
+
+    while (*(s + len) != '\0')
+        len++;
+
+    return (len);
+}
