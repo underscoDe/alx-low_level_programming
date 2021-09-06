021d6baf4ebf811f2a99766142cdac8e7611268b
underscoDe <developer.awounou@gmail.com>
2021-09-06 17:41:15 +0100

    fear lack of computers

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_puts - prints a string
+ * @str: string to print
+ *
+ * Description: prints a string
+ * On success: returns no error
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while(*(str + i) != '\0')
+		putchar(*(str + i));
+	putchar(10);
+}
