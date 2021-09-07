49248850f5280993ab097d4ff05e0efc54840b05
underscoDe <developer.awounou@gmail.com>
2021-09-07 15:01:28 +0100

    saving numbers or people

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,17 +9,17 @@
 
 int _atoi(char *s)
 {
-	int i = 1;
+	int sign = 1;
 	unsigned int num = 0;
 
 	do {
 		if (*s == '-')
-			i *= -1;
+			sign *= -1;
 		else if (*s >= '0' && *s <= '9')
 			num = num * 10 + (*s - '0');
 		else if (num > 0)
 			break;
 	} while (*s++);
 
-	return (num * i);
+	return (num * sign);
 }
