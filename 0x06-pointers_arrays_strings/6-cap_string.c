5b9f995a27de534903dd1cddb51b420bbbd54739
underscoDe <developer.awounou@gmail.com>
2021-09-10 08:32:11 +0100

    capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,9 +10,9 @@ char *cap_string(char *str)
 {
 	int index = 0;
 
-	while (str[++index])
+	while (str[index++])
 	{
-		while ((!(str[index] >= 'a') && (str[index] <= 'z')))
+		while (!(str[index] >= 'a') && (str[index] <= 'z'))
 			index++;
 
 		if (str[index - 1] == ' ' ||
