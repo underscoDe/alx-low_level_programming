57abe53ef40e15c06e03909f24d01cc3dbbd2f86
underscoDe <developer.awounou@gmail.com>
2021-09-14 21:05:28 +0100

    add prototype for print_chessboard

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -12,5 +12,6 @@ char *_strchr(char *, char);
 unsigned int _strspn(char *, char *);
 char *_strpbrk(char *, char *);
 char *_strstr(char *, char *);
+void print_chessboard(char (*a)[8]);
 
 #endif /*MAIN_H*/
