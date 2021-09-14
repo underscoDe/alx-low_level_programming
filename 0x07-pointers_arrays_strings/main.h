0be8b73d19504a159d837f8a6819ce76d64d5482
underscoDe <developer.awounou@gmail.com>
2021-09-14 21:04:57 +0100

    add prototype for strstr

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,5 +11,6 @@ char *_memcpy(char *, char *, unsigned int);
 char *_strchr(char *, char);
 unsigned int _strspn(char *, char *);
 char *_strpbrk(char *, char *);
+char *_strstr(char *, char *);
 
 #endif /*MAIN_H*/
