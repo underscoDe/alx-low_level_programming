c0844b8f52a545db654402e851a3b223239252ce
underscoDe <developer.awounou@gmail.com>
2021-09-14 21:04:35 +0100

    add prototype for strpbrk

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,5 +10,6 @@ char *_memset(char *, char, unsigned int);
 char *_memcpy(char *, char *, unsigned int);
 char *_strchr(char *, char);
 unsigned int _strspn(char *, char *);
+char *_strpbrk(char *, char *);
 
 #endif /*MAIN_H*/
