507c38699c2e8cc12289bd08a9031d69ec31d79e
underscoDe <developer.awounou@gmail.com>
2021-09-14 21:06:26 +0100

    add prototype for set_string

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -14,5 +14,6 @@ char *_strpbrk(char *, char *);
 char *_strstr(char *, char *);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *, int);
+void set_string(char **, char *);
 
 #endif /*MAIN_H*/
