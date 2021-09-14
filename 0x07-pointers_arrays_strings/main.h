9ad374a7cfa26e4eba2304b2526e1b82f0e77f5f
underscoDe <developer.awounou@gmail.com>
2021-09-14 21:05:58 +0100

    add prototype for print_diagsums

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -13,5 +13,6 @@ unsigned int _strspn(char *, char *);
 char *_strpbrk(char *, char *);
 char *_strstr(char *, char *);
 void print_chessboard(char (*a)[8]);
+void print_diagsums(int *, int);
 
 #endif /*MAIN_H*/
