19078a5de20120893093699327575ca54aac8130
underscoDe <developer.awounou@gmail.com>
2021-09-15 21:22:34 +0100

    add prototype

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int _sqrt(int n, int x);
+int is_prime(int n, int start);
+void check(char *s, int start, int end, int *flag);
+int wildcmp(char *s1, char *s2);
+
+#endif /*MAIN_H*/
