1c94fcd449d2d6588fcb5e4a04cc4abf194d4fcf
underscoDe <developer.awounou@gmail.com>
2021-09-28 19:31:46 +0100

    poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,25 +5,23 @@
 #include <stdlib.h>
 
 /**
-* struct dog - a new type of data
-* representing a dog
-* @name: name of dog
-* @age: age of dog
-* @owner: owner's name
+*struct dog - a new type of data
+*representing a dog
+*@name: name of dog
+*@age: age of dog
+*@owner: owner's name
 */
-
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+char *name;
+float age;
+char *owner;
 };
 
 
 /**
-* dog_t - Typedef for struct dog
+*dog_t - Typedef for struct dog
 */
-
 typedef struct dog dog_t;
 
 
