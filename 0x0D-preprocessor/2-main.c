c199e3fc4e838dec2af9de0734248326949d31d5
underscoDe <developer.awounou@gmail.com>
2021-09-28 18:26:00 +0100

    file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main - prints the name of
+*the file it was compiled from
+*
+*Return: nothing to be returned
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
