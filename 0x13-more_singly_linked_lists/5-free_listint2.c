9fb9957182dffccdc4fa58a6191dedf6961797b9
underscoDe <developer.awounou@gmail.com>
2021-10-14 07:07:44 +0100

    free list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,11 +1,12 @@
 #include "lists.h"
 
 /**
- * free_listint2 - frees a linked list
- * @head: pointer to the head of the list
+ *free_listint2 - frees a linked list
+ *@head: pointer to the head of the list
  *
- * Return: void
+ *Return: void
  */
+
 void free_listint2(listint_t **head)
 {
 	listint_t *cursor;
@@ -19,6 +20,7 @@ void free_listint2(listint_t **head)
 			free(cursor);
 			*head = (*head)->next;
 		}
+
 		*temp = NULL;
 	}
 }
