3fd434c8c080529cb8b7f329ad2e53fdd5c26a77
underscoDe <developer.awounou@gmail.com>
2021-10-16 06:26:14 +0100

    prototypes

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+unsigned int binary_to_uint(const char *b);
+int check_valid_string(const char *b);
+void print_binary(unsigned long int n);
+int _putchar(char c);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+
+#endif /*MAIN_H*/
