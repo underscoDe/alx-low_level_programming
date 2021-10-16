045af572277fa0ea3b1ff992e7b56b87ba84673a
underscoDe <developer.awounou@gmail.com>
2021-10-16 06:05:49 +0100

    1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * print_binary - prints the binary representaion of a number
+ * @n: number to be used
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
+}
