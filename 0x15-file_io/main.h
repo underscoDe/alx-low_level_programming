c5bcbafcc12118a0ff68fe820a73f29a6854d4a3
underscoDe <developer.awounou@gmail.com>
2021-10-22 19:21:47 +0100

    prototype

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,4 +14,4 @@ int create_file(const char *filename, char *text_content);
 int _strlen(char *str);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif /*HOLBERTON_H*/
+#endif /*MAIN_H*/
