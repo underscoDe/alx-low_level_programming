94d071f18b46412dbb73c933b7a7dea21426bb4c
Arsene Awounou <developer.awounou@gmail.com>
2021-11-29 23:53:24 +0100

    test files

---
+++ 0x18-dynamic_libraries/tests/0-main.c
@@ -0,0 +1,13 @@
+#include "../main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+	printf("%d\n", _strlen("Holberton"));
+	return (EXIT_SUCCESS);
+}
